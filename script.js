a629f7ecfac99cef21ff7872bd2b87ae21be4639
alimdavies <66089856+alimdavies@users.noreply.github.com>
2025-12-30 13:39:05 +0100

    implemented core calc mechanic

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const keyboard = document.querySelector('#keyboard')
-const display = document.querySelector('#display span')
+const display = document.querySelector('#displayLine')
+const displayExpr = document.querySelector('#displayExpr')
 
-let line = ''
+let displayLine = ''
+let expr = ''
 
 function add(a, b) {
     return a + b
@@ -16,7 +18,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b
+    return b == 0 ? 'ERROR' : a / b
 }
 
 function operate(a, b, operator) {
@@ -38,15 +40,83 @@ function operate(a, b, operator) {
     }
 }
 
+function displayValue(str) {
+    display.textContent = str
+}
+
+function uploadDisplay(str) {
+    display.textContent += str
+}
+
+function uploadExpr() {
+    displayExpr.textContent = expr
+}
+
+function clearDisplay() {
+    displayValue('')
+}
+
+let regex = /\++|\-+|\*+|\/+/
+
+function evaluate(target) {
+    const input = target.textContent
+    if(target.classList.contains('number')) {
+        if(regex.exec(expr[expr.length - 1]) != null) {
+            clearDisplay()
+            uploadDisplay(input)
+            expr += input
+            uploadExpr()
+        } else {
+            uploadDisplay(input)
+            expr += input
+            uploadExpr()
+        }
+    } else if(target.classList.contains('operator')) {
+        if(regex.exec(expr) == null) {
+            expr += input
+            uploadExpr()
+        } else if(expr == '') {
+            return
+        } else if(regex.exec(expr[expr.length - 1]) != null) {
+            return
+        } else {
+            const index = regex.exec(expr).index
+            const operator = expr[index]
+            const arr = expr.split(expr[index])
+            const result = operate(parseFloat(arr[0]), parseFloat(arr[1]), operator)
+            displayValue(result)
+            expr = `${result}${input}`
+            uploadExpr()
+        }
+    } else if(input == 'AC') {
+        clearDisplay()
+        expr = ''
+        uploadExpr()
+    } else if(input == '=') {
+        if(expr == '') {
+            return
+        } else if(regex.exec(expr[expr.length - 1]) != null) {
+            return
+        } else {
+            const index = regex.exec(expr).index
+            const operator = expr[index]
+            const arr = expr.split(expr[index])
+            const result = operate(parseFloat(arr[0]), parseFloat(arr[1]), operator)
+            displayValue(result)
+            expr = `${result}`
+        }
+    } else if(target.classList.contains('backspace')) {
+        if(expr == '') {
+            return
+        } else {
+            expr = expr.slice(0, -1)
+            uploadExpr()
+            clearDisplay()
+        }
+    }
+}
+
 keyboard.addEventListener('click', (e) => {
     e.preventDefault()
-    if(e.target.textContent == 'AC') {
-        line = ''
-        display.textContent = line
-    } else if(e.target.textContent == '=') {
-        operate()
-    } else {
-        line += e.target.textContent
-        display.textContent = line
-    }
+    evaluate(e.target)
 })
